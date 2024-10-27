0c2d1f8b16d38a6582c3d200dcb016b9cb509bde
jjangdm <jjangdm@gmail.com>
2024-10-28 05:55:44 +0900

    well done all + design modified

--- old/classes/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClassesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'classes'
-    verbose_name = '수업관리'
--- old/textbooks/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TextbooksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'textbooks'
-    verbose_name = '교재관리'
