04809be0c85043b48042b0867c7162c0270c153d
jjangdm <jjangdm@gmail.com>
2024-11-13 02:38:29 +0900

    app students dev. start

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import StudentListView
-
+from . import views
 
 app_name = 'students'
 
 urlpatterns = [
-    path('', StudentListView.as_view(), name='student_list'),
+    path('', views.StudentListView.as_view(), name='student_list'),
+    path('create/', views.StudentCreateView.as_view(), name='student_create'),
+    # ... other URL patterns
 ]
--- students/views.py
+++ students/views.py
@@ -1,7 +1,6 @@
-# from django.shortcuts import render
-
-# Create your views here.
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import Student
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -14,3 +13,13 @@ class StudentListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Student.objects.select_related('school').all()
+
+
+class StudentCreateView(LoginRequiredMixin, CreateView):
+    model = Student
+    fields = ['name', 'student_id', 'school', 'grade', 'phone_number', 'email', 
+              'gender', 'parent_phone', 'receipt_number', 'first_class_date', 
+              'quit_date', 'etc']  # Adjust fields as needed
+    template_name = 'students/student_create.html'  # Create this template
+    success_url = reverse_lazy('students:student_list') # Use reverse_lazy
+
