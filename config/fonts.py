7cdf7213b87397d523c46f85803d152b1b8eace5
jjangdm <jjangdm@gmail.com>
2024-12-01 22:14:04 +0900

    fonts 정리

--- config/fonts.py
+++ config/fonts.py
@@ -7,7 +7,24 @@ FONTS_DIR = os.path.join(settings.BASE_DIR, 'static', 'fonts')
 FONT_CONFIGS = {
     'nanum': {
         'name': 'NanumGothic',
-        'path': os.path.join(FONTS_DIR, 'NanumGothic', 'NanumGothic.ttf'),
+        'variants': {
+            'regular': {
+                'name': 'NanumGothic',
+                'path': os.path.join(FONTS_DIR, 'NanumGothic', 'NanumGothic.ttf'),
+            },
+            'bold': {
+                'name': 'NanumGothicBold',
+                'path': os.path.join(FONTS_DIR, 'NanumGothic', 'NanumGothicBold.ttf'),
+            },
+            'extra_bold': {
+                'name': 'NanumGothicExtraBold',
+                'path': os.path.join(FONTS_DIR, 'NanumGothic', 'NanumGothicExtraBold.ttf'),
+            },
+            'light': {
+                'name': 'NanumGothicLight',
+                'path': os.path.join(FONTS_DIR, 'NanumGothic', 'NanumGothicLight.ttf'),
+            },
+        },
     },
     'noto_sans': {
         'name': 'NotoSansKR',
@@ -23,11 +40,40 @@ FONT_CONFIGS = {
             'thin': {
                 'name': 'NotoSansKR-Thin',
                 'path': os.path.join(FONTS_DIR, 'Noto_Sans_KR', 'NotoSansKR-Thin.ttf'),
-            }
+            },
+            'light': {
+                'name': 'NotoSansKR-Light',
+                'path': os.path.join(FONTS_DIR, 'Noto_Sans_KR', 'NotoSansKR-Light.ttf'),
+            },
+            'medium': {
+                'name': 'NotoSansKR-Medium',
+                'path': os.path.join(FONTS_DIR, 'Noto_Sans_KR', 'NotoSansKR-Medium.ttf'),
+            },
+            'semi_bold': {
+                'name': 'NotoSansKR-SemiBold',
+                'path': os.path.join(FONTS_DIR, 'Noto_Sans_KR', 'NotoSansKR-SemiBold.ttf'),
+            },
         }
     },
     'ubuntu': {
         'name': 'Ubuntu-Regular',
-        'path': os.path.join(FONTS_DIR, 'Ubuntu', 'Ubuntu-Regular.ttf'),
-    }
+        'variants': {
+            'regular': {
+                'name': 'Ubuntu-Regular',
+                'path': os.path.join(FONTS_DIR, 'Ubuntu', 'Ubuntu-Regular.ttf'),
+            },
+            'bold': {
+                'name': 'Ubuntu-Bold',
+                'path': os.path.join(FONTS_DIR, 'Ubuntu', 'Ubuntu-Bold.ttf'),
+            },
+            'light': {
+                'name': 'Ubuntu-Light',
+                'path': os.path.join(FONTS_DIR, 'Ubuntu', 'Ubuntu-Light.ttf'),
+            },
+            'medium': {
+                'name': 'Ubuntu-Medium',
+                'path': os.path.join(FONTS_DIR, 'Ubuntu', 'Ubuntu-Medium.ttf'),
+            },
+        },
+    },
 }
